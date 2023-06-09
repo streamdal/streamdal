d70ffa5c6f1ec570fffd146e53967e0a3ff6da00
Jacob Heric <jacob@jacobheric.com:q!>
2023-06-09 17:11:51 -0400

    build it

---
+++ dist/_astro/ruleSet.1b833b33.js
@@ -0,0 +1,1 @@
+import{a as e}from"./ruleSet.db6e1c85.js";import"./index.03be2d59.js";import"./error.76753ac1.js";import"./streamdal.8a6e1b11.js";export{e as RuleSet};
---
+++ dist/_astro/utils.18ab1466.js
@@ -0,0 +1,1 @@
+const r=t=>t.replace(/\w\S*/g,e=>e.charAt(0).toUpperCase()+e.substring(1).toLowerCase());export{r as t};
