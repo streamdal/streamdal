a1b28ba9e39fbc29416259ef198da75e8d8739d3
Adelle Pitsas <apitsas4@gmail.com>
2023-07-03 13:38:18 -0400

    add scaffolding for login page

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -3,9 +3,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import type { RulesetType } from "../rules/rulesetAddEdit";
 import { LoginFormInput } from "./loginFormInput";
 import * as z from "zod";
-import { FormInput } from "../form/formInput";
 import { useState } from "react";
-import { FormHidden } from "../form/formHidden";
+import { Button, CustomFlowbiteTheme } from "flowbite-react";
+import { Github } from "../icons/social";
 
 export const loginSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
@@ -27,9 +27,18 @@ export const LoginForm = () => {
       zodResolver(loginSchema)({ ...data }, context, options),
   });
 
+  const customTheme: CustomFlowbiteTheme["button"] = {
+    color: {
+      primary: "bg:red-100 hover:bg-red-600",
+    },
+  };
+
   return (
-    <div className="p-2 max-w-lg h-full flex flex-col justify-center align-top">
-      <form>
+    <div className="p-2 w-full h-[500px] flex flex-col justify-center align-top items-center">
+      <form
+        className={"border-web border-2 rounded-xl px-8 py-10 items-center"}
+      >
+        <h2 className={"text-center mb-3 text-xl"}>Welcome Back!</h2>
         <LoginFormInput
           name={"email"}
           label={"Email"}
@@ -42,14 +51,22 @@ export const LoginForm = () => {
           register={register}
           error={errors["password"]?.message || ""}
         />
-        {/*<div className="flex flex-col">*/}
-        {/*    <label form="email">Email</label>*/}
-        {/*    <input type={"text"} name={"email"} placeholder={"Email"}/>*/}
-        {/*</div>*/}
-        {/*<div className="flex flex-col mt-8">*/}
-        {/*    <label form="password">Password</label>*/}
-        {/*    <input type={"text"} name={"password"} placeholder={"Password"}/>*/}
-        {/*</div>*/}
+        <div className={"flex flex-col items-center"}>
+          <Button
+            className={
+              "bg-streamdalYellow btn-heimdal text-web mb-3 w-full md:max-2xl:w-1/2"
+            }
+          >
+            Sign In
+          </Button>
+          <Button
+            color="gray"
+            className="text-web hover:text-web hover:bg-streamdalYellow w-full md:max-2xl:w-1/2"
+          >
+            <Github className="mr-2 h-5 w-5" />
+            <p>Login with Github</p>
+          </Button>
+        </div>
       </form>
     </div>
   );
---
+++ src/components/styles/themes.ts
@@ -0,0 +1,7 @@
+import type { CustomFlowbiteTheme } from "flowbite-react";
+
+export const buttonTheme: CustomFlowbiteTheme["button"] = {
+  color: {
+    primary: "hover:bg-gradient-to-r from-cyan-500 to-blue-500",
+  },
+};
