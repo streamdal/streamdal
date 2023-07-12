4e8e9da6a16874552440ebd6012d9c8d9fa601f7
Adelle Pitsas <apitsas4@gmail.com>
2023-07-12 11:30:36 -0400

    add login component and styles

---
+++ components/auth/loginFormInput.tsx
@@ -0,0 +1,41 @@
+import type React from "react";
+import type { InputHTMLAttributes } from "react";
+
+export type FormInputProps = {
+  name: string;
+  label: string;
+  error?: any;
+  register: any;
+  row?: boolean;
+  margin?: string;
+};
+
+export const LoginFormInput = ({
+  name,
+  label,
+  // error,
+  // register,
+  row,
+  margin = "mb-4",
+  ...props
+}: FormInputProps & InputHTMLAttributes<string>) => {
+  return (
+    <div className={`flex flex-col ${margin} w-full w-[200px]`}>
+      <div className={`flex ${row ? "flex-row" : "flex-col"} mb-1 block`}>
+        <div>
+          <label
+            for={name}
+            class="text-sm font-medium text-gray-900 dark:text-white"
+          >
+            {label}
+          </label>
+          <input
+            id={name}
+            class="bg-purple-50 border h-10 pl-3 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ routes/login.tsx
@@ -0,0 +1,12 @@
+import { PageProps } from "$fresh/src/server/types.ts";
+import { RuleSets, RuleSetType } from "../components/rules/sets.tsx";
+import { Layout } from "../components/layout.tsx";
+import { LoginForm } from "../components/auth/loginForm.tsx";
+
+export default function Home(props: PageProps<RuleSetType>) {
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  );
+}
