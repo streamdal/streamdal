73c75ab3ccec4bee457c44216f42a8dac14df924
Daniel Selans <daniel.selans@gmail.com>
2023-07-14 13:47:50 -0700

    transform wasm

--- detective/src/lib.rs
+++ detective/src/lib.rs
@@ -1,13 +1,12 @@
 use protobuf::EnumOrUnknown;
 use protos::detective::DetectiveType;
-use protos::pipeline::{WASMExitCode, WASMRequest, WASMResponse};
+use protos::pipeline::{WASMExitCode, WASMRequest};
 use snitch_detective::detective::{Detective, Request};
-use std::mem;
 
 #[no_mangle]
 pub extern "C" fn f(ptr: *mut u8, length: usize) -> *mut u8 {
     // Read request
-    let wasm_request = match read_request(ptr, length) {
+    let wasm_request = match common::read_request(ptr, length) {
         Ok(req) => req,
         Err(e) => panic!("unable to read request: {}", e), // TODO: Should write response here
     };
@@ -29,9 +28,9 @@ pub extern "C" fn f(ptr: *mut u8, length: usize) -> *mut u8 {
                 exit_code = WASMExitCode::WASM_EXIT_CODE_SUCCESS;
             }
 
-            write_response(&req.data, exit_code, "completed detective run".to_string())
+            common::write_response(&req.data, exit_code, "completed detective run".to_string())
         }
-        Err(e) => write_response(
+        Err(e) => common::write_response(
             &req.data,
             WASMExitCode::WASM_EXIT_CODE_INTERNAL_ERROR,
             e.to_string(),
@@ -69,58 +68,18 @@ fn validate_wasm_request(req: &WASMRequest) -> Result<(), String> {
     Ok(())
 }
 
-fn write_response(output: &Vec<u8>, exit_code: WASMExitCode, exit_msg: String) -> *mut u8 {
-    let mut response = WASMResponse::new();
-
-    response.output = output.to_vec();
-    response.exit_code = EnumOrUnknown::from(exit_code);
-    response.exit_msg = exit_msg;
-
-    let mut bytes = protobuf::Message::write_to_bytes(&response).unwrap();
-
-    // Append 3 terminators (ascii code 166 = ¦)
-    bytes.extend_from_slice(&[166, 166, 166]);
-
-    let ptr = bytes.as_mut_ptr();
-
-    ptr
-}
-
-fn read_request(ptr: *mut u8, length: usize) -> Result<WASMRequest, String> {
-    let request_bytes = read_memory(ptr, length);
-
-    // Decode read request
-    let request: WASMRequest =
-        protobuf::Message::parse_from_bytes(request_bytes.as_slice()).map_err(|e| e.to_string())?;
-
-    // Dealloc request bytes
-    unsafe {
-        dealloc(ptr, length as i32);
-    }
-
-    Ok(request)
-}
-
-// Should generally be called by client when generating request
+/// # Safety
+///
+/// This is unsafe because it operates on raw memory; see `common/src/lib.rs`.
 #[no_mangle]
 pub unsafe extern "C" fn alloc(size: i32) -> *mut u8 {
-    let mut buffer = Vec::with_capacity(size as usize);
-
-    let pointer = buffer.as_mut_ptr();
-
-    mem::forget(buffer);
-
-    pointer as *mut u8
+    common::alloc(size)
 }
 
-// Should generally be called by exported WASM func _after_ it is finished working with request
+/// # Safety
+///
+/// This is unsafe because it operates on raw memory; see `common/src/lib.rs`.
 #[no_mangle]
 pub unsafe extern "C" fn dealloc(pointer: *mut u8, size: i32) {
-    drop(Vec::from_raw_parts(pointer, size as usize, size as usize))
-}
-
-// Helper for reading data from memory
-fn read_memory(ptr: *mut u8, length: usize) -> Vec<u8> {
-    let array = unsafe { std::slice::from_raw_parts(ptr, length) };
-    return array.to_vec();
+    common::dealloc(pointer, size)
 }
--- transform/src/lib.rs
+++ transform/src/lib.rs
@@ -1,14 +1,92 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use protos::pipeline::{WASMExitCode, WASMRequest};
+use protos::transform::TransformType;
+use snitch_transform::transform;
+
+#[no_mangle]
+pub extern "C" fn f(ptr: *mut u8, length: usize) -> *mut u8 {
+    // Read request
+    let wasm_request = match common::read_request(ptr, length) {
+        Ok(req) => req,
+        Err(e) => panic!("unable to read request: {}", e), // TODO: Should write response here
+    };
+
+    // Validate request
+    if let Err(err) = validate_wasm_request(&wasm_request) {
+        panic!("invalid step: {}", err) // TODO: Should write response here
+    }
+
+    // Generate snitch-transform request
+    let transform_request = generate_transform_request(&wasm_request);
+
+    // Run request against snitch-transform
+    let result = match wasm_request.step.transform().type_.unwrap() {
+        TransformType::TRANSFORM_TYPE_REPLACE_VALUE => transform::overwrite(&transform_request),
+        TransformType::TRANSFORM_TYPE_MASK_VALUE => transform::mask(&transform_request),
+        TransformType::TRANSFORM_TYPE_OBFUSCATE_VALUE => transform::obfuscate(&transform_request),
+        _ => {
+            return common::write_response(
+                &wasm_request.input,
+                WASMExitCode::WASM_EXIT_CODE_FAILURE,
+                "Unknown transform type".to_string(),
+            )
+        }
+    };
+
+    // Inspect result and return potentially transformed payload
+    match result {
+        Ok(data) => common::write_response(
+            &data.into_bytes(),
+            WASMExitCode::WASM_EXIT_CODE_SUCCESS,
+            "Successfully transformed payload".to_string(),
+        ),
+        Err(err) => common::write_response(
+            &wasm_request.input,
+            WASMExitCode::WASM_EXIT_CODE_FAILURE,
+            format!("Unable to transform payload: {:?}", err),
+        ),
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn generate_transform_request(wasm_request: &WASMRequest) -> transform::Request {
+    transform::Request {
+        data: wasm_request.input.clone(),
+        path: wasm_request.step.transform().path.clone(),
+        value: wasm_request.step.transform().value.clone(),
     }
+}
+
+fn validate_wasm_request(req: &WASMRequest) -> Result<(), String> {
+    if req.input.is_empty() {
+        return Err("input cannot be empty".to_string());
+    }
+
+    if !req.step.has_transform() {
+        return Err("transform is required".to_string());
+    }
+
+    if req.step.transform().type_.unwrap() == TransformType::TRANSFORM_TYPE_UNKNOWN {
+        return Err("transform type cannot be unknown".to_string());
+    }
+
+    if req.step.transform().path.is_empty() {
+        return Err("transform path cannot be empty".to_string());
+    }
+
+    Ok(())
+}
+
+/// # Safety
+///
+/// This is unsafe because it operates on raw memory; see `common/src/lib.rs`.
+#[no_mangle]
+pub unsafe extern "C" fn alloc(size: i32) -> *mut u8 {
+    common::alloc(size)
+}
+
+/// # Safety
+///
+/// This is unsafe because it operates on raw memory; see `common/src/lib.rs`.
+#[no_mangle]
+pub unsafe extern "C" fn dealloc(pointer: *mut u8, size: i32) {
+    common::dealloc(pointer, size)
 }
