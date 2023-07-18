d3c068a8a62093c7c0095066ca48413c422f9165
Jacob Heric <jacob@jacobheric.com:q!>
2023-07-18 15:15:25 -0400

    try adding some meta headers

--- routes/grpc.tsx
+++ routes/grpc.tsx
@@ -1,10 +1,15 @@
 import { Layout } from "../components/layout.tsx";
-import { TestRequest } from "../lib/protos/external_api.ts";
 import { ExternalClient } from "../lib/protos/external_api.client.ts";
 import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
 
 const transport = new GrpcWebFetchTransport({
   baseUrl: "http://localhost:9091",
+  format: "binary",
+  fetchInit: {
+    meta: {
+      "auth_token": "1234",
+    },
+  },
 });
 
 const client = new ExternalClient(transport);
@@ -13,10 +18,12 @@ try {
   // not 100% sure about the test request format
   // const input = new TextEncoder().encode(JSON.stringify("test grpc-web call"));
   // const r = TestRequest.create({ input });
-  const { response } = await client.test({ input: "test" });
+  const { response } = await client.test({ input: "hello world" }, {
+    meta: { "auth_token": "1234" },
+  });
   console.log("test response:", response);
 } catch (error) {
-  console.log("shit error", error);
+  console.log("error", error);
 }
 
 export default function GRPC() {
