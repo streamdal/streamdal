a2d1a13dd9bf81bf3eac3f18869ae8cf6e0aaf8e
Jacob Heric <jacob@jacobheric.com:q!>
2023-07-19 16:26:12 -0400

    gix path issue with deno build

--- build/ts/esm_module_extensions.js
+++ build/ts/esm_module_extensions.js
@@ -5,7 +5,10 @@ import path from 'path';
 const files = await glob("./protos/deno/**/*.ts")
 
 files.forEach(file => {
-  const dir = `./deno/${path.dirname(file)}`;
+  //
+  // going from /protos/deno to /deno/protos
+  const f = file.replace("deno/", "");
+  const dir = `./deno/${path.dirname(f)}`;
   const content = fs.readFileSync(file, 'utf-8')
     .split('\n').map(s => s.replace(/^(import .+? from ["']\..+?)(["'];)$/, '$1.ts$2'))
     .join('\n');
@@ -13,5 +16,5 @@ files.forEach(file => {
   if (!fs.existsSync(dir)){
     fs.mkdirSync(dir, { recursive: true });
   }
-  fs.writeFileSync(`./deno/${file}`, content, 'utf-8')
+  fs.writeFileSync(`./deno/${f}`, content, 'utf-8')
 });
