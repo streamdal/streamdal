4991d9dc810e513ca9cbb20e50262ed604faeed2
Adelle Pitsas <apitsas4@gmail.com>
2023-07-27 13:56:52 -0400

    add new menu items and icons

--- components/modals/InfoModal.tsx
+++ components/modals/InfoModal.tsx
@@ -1,76 +1,77 @@
 export const InfoModal = () => {
-  return (
-    <div>
-      <div
-        className={"absolute w-screen h-full mt-[-60px] bg-black opacity-20 z-30"}
-      >
-      </div>
-      <div
-        tabIndex="-1"
-        class="absolute mt-20 right-0 z-50 w-[200px] h-[300px] p-4 overflow-x-hidden overflow-y-hidden md:inset-0 max-h-full"
-      >
-        <div class="relative w-full max-w-2xl max-h-full">
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-              <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                Terms of Service
-              </h3>
-              <button
-                type="button"
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-hide="defaultModal"
-              >
-                <svg
-                  className="w-3 h-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                  />
-                </svg>
-                <span class="sr-only">Close modal</span>
-              </button>
+    return (
+        <div>
+            {/*<div*/}
+            {/*    className={"absolute w-screen h-full mt-[-60px] mb-[-20px] bg-black opacity-20 z-30"}*/}
+            {/*>*/}
+            {/*</div>*/}
+            <div
+                tabIndex="-1"
+                class="absolute mt-20 right-10 z-50 w-[308px] h-[560px] p-4 overflow-x-hidden overflow-y-hidden"
+            >
+                <div class="relative w-full max-w-2xl max-h-full">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                Terms of Service
+                            </h3>
+                            <button
+                                type="button"
+                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="defaultModal"
+                            >
+                                <svg
+                                    className="w-3 h-3"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 14 14"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                    />
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <div class="p-6 space-y-6">
+                            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                With less than a month to go before the European Union enacts
+                                new consumer privacy laws for its citizens, companies around the
+                                world are updating their terms of service agreements to comply.
+                            </p>
+                            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                The European Union’s General Data Protection Regulation
+                                (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
+                                common set of data rights in the European Union. It requires
+                                organizations to notify users as soon as possible of high-risk
+                                data breaches that could personally affect them.
+                            </p>
+                        </div>
+                        <div
+                            class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                            <button
+                                data-modal-hide="defaultModal"
+                                type="button"
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >
+                                I accept
+                            </button>
+                            <button
+                                data-modal-hide="defaultModal"
+                                type="button"
+                                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                            >
+                                Decline
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="p-6 space-y-6">
-              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                With less than a month to go before the European Union enacts
-                new consumer privacy laws for its citizens, companies around the
-                world are updating their terms of service agreements to comply.
-              </p>
-              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                The European Union’s General Data Protection Regulation
-                (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
-                common set of data rights in the European Union. It requires
-                organizations to notify users as soon as possible of high-risk
-                data breaches that could personally affect them.
-              </p>
-            </div>
-            <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-              <button
-                data-modal-hide="defaultModal"
-                type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                I accept
-              </button>
-              <button
-                data-modal-hide="defaultModal"
-                type="button"
-                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-              >
-                Decline
-              </button>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
--- routes/flow/info.tsx
+++ routes/flow/info.tsx
@@ -1,17 +1,22 @@
-import { Layout } from "../../components/layout.tsx";
+import {Layout} from "../../components/layout.tsx";
 import Flow from "../../islands/flow.tsx";
 import {
-  ReactFlowProvider,
+    ReactFlowProvider,
 } from "https://esm.sh/v128/@reactflow/core@11.7.4/X-YS9AdHlwZXMvcmVhY3Q6cHJlYWN0L2NvbXBhdCxyZWFjdC1kb206cHJlYWN0L2NvbXBhdCxyZWFjdDpwcmVhY3QvY29tcGF0CmUvcHJlYWN0L2NvbXBhdA/denonext/core.mjs";
-import { InfoModal } from "../../components/modals/InfoModal.tsx";
+import {InfoModal} from "../../components/modals/InfoModal.tsx";
+import {PageProps} from "$fresh/src/server/types.ts";
 
-export default function FlowRoute() {
-  return (
-    <Layout>
-      <InfoModal />
-      <ReactFlowProvider>
-        <Flow />
-      </ReactFlowProvider>
-    </Layout>
-  );
+// this route will be dynamically building based on what node is clicked
+// 
+
+export default function FlowRoute(props: PageProps) {
+    console.log(props)
+    return (
+        <Layout>
+            <InfoModal/>
+            <ReactFlowProvider>
+                <Flow/>
+            </ReactFlowProvider>
+        </Layout>
+    );
 }
