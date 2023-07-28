6c7fdebf501f9b3c1b005ad0d003e08bca2c6627
Jacob Heric <jacob@jacobheric.com:q!>
2023-07-28 10:36:48 -0400

    it works

--- routes/flow.tsx
+++ routes/flow.tsx
@@ -5,6 +5,20 @@ import Flow from "../islands/flow.tsx";
 import {
   ReactFlowProvider,
 } from "https://esm.sh/v128/@reactflow/core@11.7.4/X-YS9AdHlwZXMvcmVhY3Q6cHJlYWN0L2NvbXBhdCxyZWFjdC1kb206cHJlYWN0L2NvbXBhdCxyZWFjdDpwcmVhY3QvY29tcGF0CmUvcHJlYWN0L2NvbXBhdA/denonext/core.mjs";
+import { client } from "./grpc.tsx";
+
+try {
+  // const { response } = await client.test({ input: "hello world" }, {
+  //   meta: { "auth-token": "1234" },
+  // });
+  const { response } = await client.getServiceMap({}, {
+    meta: { "auth-token": "1234" },
+  });
+
+  console.log("test response:", response);
+} catch (error) {
+  console.log("error", error);
+}
 
 export default function FlowRoute() {
   return (
