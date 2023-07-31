86ad4b7028a94c41c94a9461c510fc5805a836dc
Adelle Pitsas <apitsas4@gmail.com>
2023-07-31 15:28:55 -0400

    dynamically render nodes

--- islands/flow.tsx
+++ islands/flow.tsx
@@ -5,43 +5,46 @@ import {
     useNodesState,
 } from "https://esm.sh/v128/@reactflow/core@11.7.4/X-YS9AdHlwZXMvcmVhY3Q6cHJlYWN0L2NvbXBhdCxyZWFjdC1kb206cHJlYWN0L2NvbXBhdCxyZWFjdDpwcmVhY3QvY29tcGF0CmUvcHJlYWN0L2NvbXBhdA/denonext/core.mjs";
 import "flowbite";
+import { useEffect } from "https://esm.sh/preact@10.15.1/hooks";
 
-const initialNodes = [
-    {
-        id: "1",
-        type: "service",
-        dragHandle: "#dragHandle",
-        position: {x: 150, y: 0},
-        data: {label: "Service"},
-    },
-    {
-        id: "2",
-        type: "participants",
-        dragHandle: "#dragHandle",
-        position: {x: 50, y: 200},
-        zIndex: 2,
-        data: {label: "Consumer", source: "top", target: "bottom"},
-    },
-    {
-        id: "3",
-        type: "participants",
-        dragHandle: "#dragHandle",
-        position: {x: 325, y: 200},
-        zIndex: 2,
-        data: {label: "Producer", source: "bottom", target: "top"},
-    },
-    {
-        id: "4",
-        type: "platform",
-        sourcePosition: "right",
-        targetPosition: "left",
-        position: {x: 215, y: 350},
-    },
-];
+
+// const initialNodes = [
+//     {
+//         id: "1",
+//         type: "service",
+//         dragHandle: "#dragHandle",
+//         position: {x: 150, y: 0},
+//         data: {label: "testing"},
+//     },
+//     {
+//         id: "2",
+//         type: "participants",
+//         dragHandle: "#dragHandle",
+//         position: {x: 50, y: 200},
+//         zIndex: 2,
+//         data: {label: "Consumer", source: "top", target: "bottom"},
+//     },
+//     {
+//         id: "3",
+//         type: "participants",
+//         dragHandle: "#dragHandle",
+//         position: {x: 325, y: 200},
+//         zIndex: 2,
+//         data: {label: "Producer", source: "bottom", target: "top"},
+//     },
+//     {
+//         id: "4",
+//         type: "platform",
+//         sourcePosition: "right",
+//         targetPosition: "left",
+//         position: {x: 215, y: 350},
+//     },
+// ];
+
 const initialEdges = [
     {
         id: "e1-2",
-        source: "2",
+        source: "2000",
         target: "1",
         targetHandle: "c",
         sourceHandle: "a",
@@ -108,12 +111,88 @@ const initialEdges = [
 
 const nodeTypes = {
     platform: Platform,
-    service: Service,
+    producer: Producer,
+    consumer: Consumer,
     participants: Participants,
 };
+
 export default function Flow(props: any) {
-    console.log("these here be the props", props)
-    const [nodes, , onNodesChange] = useNodesState(initialNodes);
+    // console.log("data we want", props.props)
+
+    // console.log("these here be the props", props)
+    const [nodes, setNodes , onNodesChange] = useNodesState([]);
+    
+    useEffect(() => {
+        const keys = Object.keys(props.props.serviceMap)
+        const serviceMap = props.props.serviceMap
+        console.log("useEffect", serviceMap[keys[0]])
+        let nodes = [
+            {
+                id: "1",
+                type: "service",
+                dragHandle: "#dragHandle",
+                position: {x: 150, y: 0},
+                data: {label: `${serviceMap[keys[0]].name}`},
+            },
+            {
+                id: "10",
+                type: "platform",
+                sourcePosition: "right",
+                targetPosition: "left",
+                position: {x: 215, y: 350},
+                data: {label: `${serviceMap[keys[0]].pipelines[0].audience.componentName}`}
+            },
+        ]
+        setNodes([...nodes, getProducers(serviceMap[keys[0]].producers), getConsumers(serviceMap[keys[0]].consumers)])
+    }, [])
+
+    useEffect(() => {
+
+    }, [nodes])
+
+    const getProducers = (prod : any) => {
+        if (prod.length === 0) {
+            return {
+                id: "1000",
+                type: "participants",
+                dragHandle: "#dragHandle",
+                position: {x: 325, y: 200},
+                zIndex: 2,
+                data: {label: "Producer", source: "bottom", target: "top"},
+            }
+        } else {
+            return {
+                id: "1000",
+                type: "participants",
+                dragHandle: "#dragHandle",
+                position: {x: 325, y: 200},
+                zIndex: 2,
+                data: {label: "Producer", source: "bottom", target: "top"},
+            }
+        }
+    }
+
+    const getConsumers = (prod : any) => {
+        if (prod.length === 0) {
+            return {
+                id: "2000",
+                type: "participants",
+                dragHandle: "#dragHandle",
+                position: {x: 50, y: 200},
+                zIndex: 2,
+                data: {label: "Consumer", source: "bottom", target: "top"},
+            }
+        } else {
+            return {
+                id: "2000",
+                type: "participants",
+                dragHandle: "#dragHandle",
+                position: {x: 50, y: 200},
+                zIndex: 2,
+                data: {label: "Consumer", source: "bottom", target: "top"},
+            }
+        }
+    }
 
     return (
         <div
--- routes/flow/index.tsx
+++ routes/flow/index.tsx
@@ -6,6 +6,7 @@ import {
   ReactFlowProvider,
 } from "https://esm.sh/v128/@reactflow/core@11.7.4/X-YS9AdHlwZXMvcmVhY3Q6cHJlYWN0L2NvbXBhdCxyZWFjdC1kb206cHJlYWN0L2NvbXBhdCxyZWFjdDpwcmVhY3QvY29tcGF0CmUvcHJlYWN0L2NvbXBhdA/denonext/core.mjs";
 import { client } from "../grpc.tsx";
+import { useEffect, useState } from "https://esm.sh/preact@10.15.1/hooks";
 
 interface ServiceMap {
   serviceMap: any;
@@ -26,7 +27,7 @@ export const handler: Handlers<ServiceMap> = {
     },
 };
 
-export default function FlowRoute({data} : {data: any}) {
+export default function FlowRoute({data}: any) {
   return (
     <Layout>
       <ReactFlowProvider>
