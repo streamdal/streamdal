129d8965fbfb0ba53423470ef8448c1fb4b4444d
Daniel Selans <daniel.selans@gmail.com>
2023-08-02 11:07:25 -0700

    auth tests done

--- apis/grpcapi/external_test.go
+++ apis/grpcapi/external_test.go
@@ -15,62 +15,293 @@ import (
 	"github.com/streamdal/snitch-server/deps"
 )
 
+const (
+	AuthToken = "1234"
+)
+
+var (
+	ctxWithNoAuth   = context.Background()
+	ctxWithBadAuth  = metadata.NewOutgoingContext(context.Background(), metadata.New(map[string]string{"auth-token": "incorrect"}))
+	ctxWithGoodAuth = metadata.NewOutgoingContext(context.Background(), metadata.New(map[string]string{"auth-token": AuthToken}))
+)
+
 func init() {
 	go runServer()
 	time.Sleep(time.Second)
 }
 
+type AuthTest struct {
+	Name               string
+	Func               func() error
+	ShouldError        bool
+	ErrorShouldContain string
+}
+
 var _ = Describe("External gRPC API", func() {
 	var (
-		err    error
-		client protos.ExternalClient
-
-		authMd      = metadata.New(map[string]string{"auth-token": "1234"})
-		ctxWithAuth = metadata.NewOutgoingContext(context.Background(), authMd)
+		err            error
+		externalClient protos.ExternalClient
 	)
 
 	BeforeEach(func() {
-		client, err = newClient()
+		externalClient, err = newExternalClient()
 		Expect(err).ToNot(HaveOccurred())
-		Expect(client).ToNot(BeNil())
+		Expect(externalClient).ToNot(BeNil())
 	})
 
-	FDescribe("Test", func() {
+	Describe("Auth", func() {
+		It("auth should be enforced", func() {
+			authTests := []AuthTest{
+				{
+					Name: "External.Test without auth",
+					Func: func() error {
+						_, err := externalClient.Test(ctxWithNoAuth, &protos.TestRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "External.Test with bad auth",
+					Func: func() error {
+						_, err := externalClient.Test(ctxWithBadAuth, &protos.TestRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "invalid auth token",
+				},
+				{
+					Name: "External.Test with good auth",
+					Func: func() error {
+						_, err := externalClient.Test(ctxWithGoodAuth, &protos.TestRequest{})
+						return err
+					},
+					ShouldError: false,
+				},
+				// Don't think we need to test every auth mutation from this point;
+				// test that auth is enabled on every endpoint
+				{
+					Name: "External.GetPipeline without auth",
+					Func: func() error {
+						_, err := externalClient.GetPipeline(ctxWithNoAuth, &protos.GetPipelineRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "External.GetPipelines without auth",
+					Func: func() error {
+						_, err := externalClient.GetPipelines(ctxWithNoAuth, &protos.GetPipelinesRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "External.CreatePipeline without auth",
+					Func: func() error {
+						_, err := externalClient.CreatePipeline(ctxWithNoAuth, &protos.CreatePipelineRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "External.DeletePipeline without auth",
+					Func: func() error {
+						_, err := externalClient.DeletePipeline(ctxWithNoAuth, &protos.DeletePipelineRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "External.UpdatePipeline without auth",
+					Func: func() error {
+						_, err := externalClient.UpdatePipeline(ctxWithNoAuth, &protos.UpdatePipelineRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "External.AttachPipeline without auth",
+					Func: func() error {
+						_, err := externalClient.AttachPipeline(ctxWithNoAuth, &protos.AttachPipelineRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "External.DetachPipeline without auth",
+					Func: func() error {
+						_, err := externalClient.DetachPipeline(ctxWithNoAuth, &protos.DetachPipelineRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "External.GetServiceMap without auth",
+					Func: func() error {
+						_, err := externalClient.GetServiceMap(ctxWithNoAuth, &protos.GetServiceMapRequest{})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+			}
+
+			for _, a := range authTests {
+				err := a.Func()
+				if a.ShouldError {
+					Expect(err).To(HaveOccurred(), a.Name)
+					Expect(err.Error()).To(ContainSubstring(a.ErrorShouldContain), a.Name)
+				} else {
+					Expect(err).ToNot(HaveOccurred(), a.Name)
+				}
+			}
+		})
+	})
+
+	Describe("Test", func() {
 		It("should return pong", func() {
-			resp, err := client.Test(ctxWithAuth, &protos.TestRequest{Input: "foo"})
+			resp, err := externalClient.Test(ctxWithGoodAuth, &protos.TestRequest{Input: "foo"})
 			Expect(err).ToNot(HaveOccurred())
 			Expect(resp).ToNot(BeNil())
 			Expect(resp.Output).To(Equal("Pong: foo"))
 		})
+	})
 
-		It("should error without auth token", func() {
-			resp, err := client.Test(context.Background(), &protos.TestRequest{Input: "foo"})
-			Expect(err).To(HaveOccurred())
-			Expect(resp).To(BeNil())
-		})
+	Describe("CreatePipeline", func() {
+	})
 
-		It("should error with invalid auth token", func() {
-			authMd = metadata.New(map[string]string{"auth-token": "incorrect"})
-			badCtx := metadata.NewOutgoingContext(context.Background(), authMd)
+	Describe("GetPipeline", func() {
+	})
 
-			resp, err := client.Test(badCtx, &protos.TestRequest{Input: "foo"})
-			Expect(err).To(HaveOccurred())
-			Expect(resp).To(BeNil())
-			Expect(err.Error()).To(ContainSubstring("invalid auth token"))
+	Describe("GetPipelines", func() {
+	})
+
+	Describe("UpdatePipeline", func() {
+	})
+
+	Describe("DeletePipeline", func() {
+	})
+
+	Describe("AttachPipeline", func() {
+	})
+
+	Describe("DetachPipeline", func() {
+	})
+
+	Describe("PausePipeline", func() {
+	})
+
+	Describe("ResumePipeline", func() {
+	})
+})
+
+var _ = Describe("Internal gRPC API", func() {
+	var (
+		err            error
+		internalClient protos.InternalClient
+	)
+
+	BeforeEach(func() {
+		internalClient, err = newInternalClient()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(internalClient).ToNot(BeNil())
+	})
+
+	FDescribe("Auth", func() {
+		It("auth should be enforced", func() {
+			authTests := []AuthTest{
+				{
+					Name: "Internal.Heartbeat without auth",
+					Func: func() error {
+						_, err := internalClient.Heartbeat(ctxWithNoAuth, &protos.HeartbeatRequest{SessionId: "foo"})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "Internal.Heartbeat with bad auth",
+					Func: func() error {
+						_, err := internalClient.Heartbeat(ctxWithBadAuth, &protos.HeartbeatRequest{SessionId: "foo"})
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "invalid auth token",
+				},
+				{
+					Name: "Internal.Heartbeat with good auth",
+					Func: func() error {
+						_, err := internalClient.Heartbeat(ctxWithGoodAuth, &protos.HeartbeatRequest{SessionId: "foo"})
+						return err
+					},
+					ShouldError: false,
+				},
+				// Not testing every auth case anymore, just testing base auth
+				// for each gRPC method
+
+				{
+					Name: "Internal.Register without auth",
+					Func: func() error {
+						// Register() is a streaming RPC so we need to test it differently
+						registerClient, err := internalClient.Register(ctxWithNoAuth, &protos.RegisterRequest{})
+						Expect(err).ToNot(HaveOccurred())
+
+						// Auth error won't happen until we try to recv
+						_, err = registerClient.Recv()
+
+						return err
+					},
+					ShouldError:        true,
+					ErrorShouldContain: "missing auth token",
+				},
+				{
+					Name: "Internal.NewAudience without auth",
+					Func: func() error {
+						_, err := internalClient.NewAudience(ctxWithNoAuth, &protos.NewAudienceRequest{})
+						return err
+					},
+					ShouldError: true,
+				},
+				{
+					Name: "Internal.Notify without auth",
+					Func: func() error {
+						_, err := internalClient.Notify(ctxWithNoAuth, &protos.NotifyRequest{})
+						return err
+					},
+					ShouldError: true,
+				},
+				{
+					Name: "Internal.Metrics without auth",
+					Func: func() error {
+						_, err := internalClient.Metrics(ctxWithNoAuth, &protos.MetricsRequest{})
+						return err
+					},
+					ShouldError: true,
+				},
+			}
+
+			for _, a := range authTests {
+				err := a.Func()
+				if a.ShouldError {
+					Expect(err).To(HaveOccurred(), a.Name)
+					Expect(err.Error()).To(ContainSubstring(a.ErrorShouldContain), a.Name)
+				} else {
+					Expect(err).ToNot(HaveOccurred(), a.Name)
+				}
+			}
 		})
 	})
 
 	Describe("Register", func() {
-		It("should register a new client", func() {
-
-		})
-
-		It("should error without auth token", func() {
-
-		})
-
-		It("should error with invalid auth token", func() {
-
+		It("should register a new externalClient", func() {
 		})
 
 		It("should error with no session ID", func() {
@@ -109,13 +340,19 @@ var _ = Describe("External gRPC API", func() {
 
 		})
 	})
+
+	Describe("Metrics", func() {
+	})
+
+	Describe("Notify", func() {
+	})
 })
 
 func runServer() {
 	d, err := deps.New("", &config.Config{
 		Debug:                true,
 		NodeName:             "test",
-		AuthToken:            "1234",
+		AuthToken:            AuthToken,
 		HTTPAPIListenAddress: ":9091",
 		GRPCAPIListenAddress: ":9090",
 		NATSURL:              []string{"localhost:4222"},
@@ -132,11 +369,20 @@ func runServer() {
 	}
 }
 
-func newClient() (protos.ExternalClient, error) {
+func newExternalClient() (protos.ExternalClient, error) {
 	conn, err := grpc.Dial(":9090", grpc.WithInsecure())
 	if err != nil {
 		return nil, errors.Wrap(err, "can not connect with server")
 	}
 
 	return protos.NewExternalClient(conn), nil
+}
+
+func newInternalClient() (protos.InternalClient, error) {
+	conn, err := grpc.Dial(":9090", grpc.WithInsecure())
+	if err != nil {
+		return nil, errors.Wrap(err, "can not connect with server")
+	}
+
+	return protos.NewInternalClient(conn), nil
 }
