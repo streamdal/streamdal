f478835e45ce921130032c37772ab984594321c3
Adelle Pitsas <apitsas4@gmail.com>
2023-08-09 09:10:22 -0400

    add navigation back to home

--- components/expandedNodes/expandedNodes.tsx
+++ components/expandedNodes/expandedNodes.tsx
@@ -1,21 +1,47 @@
-import {Consumer, Producer} from "../customNodes.tsx";
+import { Consumer, Producer } from "../customNodes.tsx";
+import IconX from "tabler-icons/tsx/x.tsx";
+import { useRef } from "preact/denonext/hooks.js";
 
-export const ExpandedNodes = ({nodes, params}) => {
-    const actorType = params.operationType === "consumer" ? 1 : 2
-    const filteredNodes = nodes.filter(item => item.audience.operationType === actorType).map(actor => {
-        return {label: actor.audience.operationName, pipeline: actor, flow: false}
-    })
-    console.log(filteredNodes)
-    const expandedNodes = filteredNodes.map(item => {
-        return actorType === "consumer" ? <Consumer data={...item}/> : <Producer data={...item}/>
-    })
+export const ExpandedNodes = ({ nodes, params }) => {
+  const actorType = params.operationType === "consumer" ? 1 : 2;
+  const filteredNodes = nodes.filter((item) =>
+    item.audience.operationType === actorType
+  ).map((actor) => {
+    return {
+      label: actor.audience.operationName,
+      pipeline: actor,
+      flow: false,
+    };
+  });
 
-    return (
-        <div class={"w-screen h-screen absolute top-0 bottom-0 scroll-none inset-0 flex justify-center items-center"}>
-            <div class={"z-50 bg-gray-900 opacity-30 absolute w-full h-full"}></div>
-            <div class={"grid grid-cols-2 w-fit gap-4 z-50 absolute"}>
-                {expandedNodes}
-            </div>
+  const toClose = useRef(null);
+
+  return (
+    <div
+      class={"w-screen h-screen absolute top-0 bottom-0 scroll-none inset-0 flex justify-center items-center"}
+    >
+      <div class={"z-50 bg-gray-900 opacity-30 absolute w-full h-full"}></div>
+      <div
+        class={"absolute z-50 h-1/2 flex flex-col justify-center items-center"}
+      >
+        <div class={"w-full flex justify-end mr-[-50px]"}>
+          <a href="/">
+            <button
+              type="button"
+              className="text-gray-900 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
+            >
+              <IconX class="w-6 h-6" />
+            </button>
+          </a>
         </div>
-    )
-}
+        <div class={"grid grid-cols-2 w-fit gap-4 z-50"} ref={toClose}>
+          {filteredNodes.map((item) => {
+            return actorType === "consumer"
+              ? <Consumer data={item} />
+              : <Producer data={item} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
