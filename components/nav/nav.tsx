094cb0f76fd796d25e1ed7577467c7daee9daeec
Adelle Pitsas <apitsas4@gmail.com>
2023-08-28 13:26:51 -0400

    shorted top nav

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -2,13 +2,15 @@ import { NavMenu } from "./navMenu.tsx";
 
 export const NavBar = () => {
   return (
-    <div className="sticky z-50 top-4 mx-4 px-4 flex flex-row justify-between h-16 bg-white border-1 border-gray-100 shadow-md items-center rounded">
+    <div className="sticky z-50 px-4 flex flex-row justify-between w-64 h-16 bg-white border-1 border-gray-100 shadow-md items-center rounded-br-lg
+
+">
       <div className={"flex items-center"}>
         <NavMenu />
         <a href="/" className={""}>
           <img
             src="/images/snitch-logo.svg"
-            className="w-44 h-fit ml-4 my-2"
+            className="w-44 h-fit ml-3 my-2"
           />
         </a>
       </div>
