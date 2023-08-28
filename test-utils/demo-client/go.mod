ffdeebf6126b753d4faa67fa2aa9d0c977361821
Daniel Selans <daniel.selans@gmail.com>
2023-08-27 18:48:17 -0700

    color before tables

--- test-utils/demo-client/go.mod
+++ test-utils/demo-client/go.mod
@@ -4,14 +4,18 @@ go 1.20
 
 require (
 	github.com/alecthomas/kong v0.8.0
+	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/streamdal/snitch-go-client v0.0.29
 )
 
 require (
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.1.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/relistan/go-director v0.0.0-20200406104025-dbbf5d95248d // indirect
 	github.com/streamdal/snitch-protos v0.0.67 // indirect
 	github.com/tetratelabs/wazero v1.2.0 // indirect
--- test-utils/demo-client/register.go
+++ test-utils/demo-client/register.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"context"
+	"fmt"
 	"os"
 	"time"
 
+	"github.com/fatih/color"
+	"github.com/hokaccha/go-prettyjson"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/streamdal/snitch-go-client"
@@ -120,7 +123,7 @@ func (r *Register) runClient() error {
 		DryRun:          false,
 		ShutdownCtx:     context.Background(), // TODO: Why is this required?
 		Logger:          r.log,
-		ClientType:      0, // TODO: Why do we have this here?
+		ClientType:      0, // This is intended primarily for shims - shims will specify that they're a shim
 		Audiences:       nil,
 	})
 	if err != nil {
@@ -139,22 +142,68 @@ func (r *Register) runClient() error {
 		// Consumer will have input - read input data
 		input := <-r.inputCh
 
-		llog.Debugf("Process() input: %s", input)
+		r.displayPre(input)
 
-		_, err := sc.Process(context.Background(), &snitch.ProcessRequest{
+		resp, err := sc.Process(context.Background(), &snitch.ProcessRequest{
 			ComponentName: r.config.Register.ComponentName,
 			OperationType: snitch.OperationType(r.config.Register.OperationType),
 			OperationName: r.config.Register.OperationName,
 			Data:          input,
 		})
 
-		llog.Debug("Process() after exec")
-
 		if err != nil {
-			r.log.Errorf("failed to process data: %s", err)
+			r.displayPost(resp, err)
 			return errors.Wrap(err, "failed to process data")
 		}
 
-		llog.Debug("Process() after err check")
+		r.displayPost(resp, nil)
 	}
+}
+
+func (r *Register) displayPre(data []byte) {
+	now := time.Now()
+	bold := color.New(color.Bold).SprintFunc()
+
+	fmt.Printf("========================== [%s] =========================\n\n", now.Format(time.RFC1123))
+	fmt.Println(bold(">> DATA PRE-SNITCH:"))
+
+	formatted, err := prettyjson.Format(data)
+	if err != nil {
+		r.log.Debugf("failed to format data: %s", err)
+
+		// Format failed, just print raw data
+		formatted = data
+	}
+
+	fmt.Printf("%s\n\n", formatted)
+}
+
+func (r *Register) displayPost(resp *snitch.ProcessResponse, err error) {
+	status := color.GreenString("SUCCESS")
+	statusMessage := color.GreenString(resp.Message)
+
+	bold := color.New(color.Bold).SprintFunc()
+
+	if err != nil {
+		status = color.RedString("FAILURE")
+		statusMessage = color.RedString(err.Error())
+	}
+
+	if resp.Error {
+		status = color.RedString("FAILURE")
+		statusMessage = color.RedString(resp.Message)
+	}
+
+	fmt.Printf(bold(">> STATUS: ")+"%s\n", status)
+	fmt.Printf(bold(">> MESSAGE: ")+"%s\n", statusMessage)
+
+	formatted, err := prettyjson.Format(resp.Data)
+	if err != nil {
+		r.log.Debugf("failed to format data: %s", err)
+
+		// Format failed, just print raw data
+		formatted = resp.Data
+	}
+
+	fmt.Printf(bold(">> DATA POST-SNITCH: ")+"\n%s\n\n", formatted)
 }
