a38fb7fe90c3520647865d8dad52d776ac852e5c
Jacob Heric <jacob@jacobheric.com:q!>
2023-08-29 14:00:56 -0400

    remove debug

--- components/form/validate.ts
+++ components/form/validate.ts
@@ -8,9 +8,7 @@ export const validate = <T>(
   schema: ZodSchema<T>,
   data: FormData,
 ): { data: z.infer<typeof schema> | null; errors: ErrorType | null } => {
-  try {
-    console.log("shit get", data.get("id"));
-    console.log("shit get", data.get("name"));
+  try {);
     const validated = schema.parse(data);
     return { data: validated, errors: null };
   } catch (error: any) {
