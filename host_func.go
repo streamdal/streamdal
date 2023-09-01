22e002f60f81dcf8bea6f83b91c14edc2cb737d6
Daniel Selans <daniel.selans@gmail.com>
2023-08-31 18:39:55 -0700

    renaming http helper func

--- host_func.go
+++ host_func.go
@@ -14,6 +14,12 @@ import (
 
 // HostFuncKVExists is function that is exported to and called from a Rust WASM module
 func (s *Snitch) HostFuncKVExists(_ context.Context, module api.Module, ptr, length int32) int32 {
+	// Read request
+
+	// Perform operation
+
+	// Return response
+
 	return 0
 }
 
@@ -22,29 +28,29 @@ func HostFuncHTTPRequest(_ context.Context, module api.Module, ptr, length int32
 	request := &steps.HttpRequest{}
 
 	if err := ReadRequestFromMemory(module, request, ptr, length); err != nil {
-		return httpResponse(module, http.StatusInternalServerError, "unable to read HTTP request params: "+err.Error(), nil)
+		return writeHTTPResponse(module, http.StatusInternalServerError, "unable to read HTTP request params: "+err.Error(), nil)
 	}
 
 	httpReq, err := http.NewRequest(methodFromProto(request.Method), request.Url, bytes.NewReader(request.Body))
 	if err != nil {
 		err = errors.Wrap(err, "unable to create http request")
-		return httpResponse(module, http.StatusInternalServerError, err.Error(), nil)
+		return writeHTTPResponse(module, http.StatusInternalServerError, err.Error(), nil)
 	}
 
 	resp, err := http.DefaultClient.Do(httpReq)
 	if err != nil {
 		err = errors.Wrap(err, "unable to perform http request")
-		return httpResponse(module, http.StatusInternalServerError, err.Error(), nil)
+		return writeHTTPResponse(module, http.StatusInternalServerError, err.Error(), nil)
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return httpResponse(module, http.StatusInternalServerError, err.Error(), nil)
+		return writeHTTPResponse(module, http.StatusInternalServerError, err.Error(), nil)
 	}
 
 	if resp.StatusCode > 299 {
-		return httpResponse(module, resp.StatusCode, string(body), nil)
+		return writeHTTPResponse(module, resp.StatusCode, string(body), nil)
 	}
 
 	// Get all headers from the response
@@ -53,10 +59,11 @@ func HostFuncHTTPRequest(_ context.Context, module api.Module, ptr, length int32
 		headers[k] = strings.Join(v, ", ")
 	}
 
-	return httpResponse(module, resp.StatusCode, string(body), headers)
+	return writeHTTPResponse(module, resp.StatusCode, string(body), headers)
 }
 
-func httpResponse(module api.Module, code int, body string, headers map[string]string) int32 {
+// writeHTTPResponse is a helper for HostFuncHTTPRequest()
+func writeHTTPResponse(module api.Module, code int, body string, headers map[string]string) int32 {
 	if headers == nil {
 		headers = make(map[string]string)
 	}
