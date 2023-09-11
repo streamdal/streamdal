45a95b6ca5b97bc99cd2ad85ce6ef6023ac37081
Daniel Selans <daniel.selans@gmail.com>
2023-09-10 20:20:18 -0700

    updated init_wasm.sh to show and track version

--- init_wasm.sh
+++ init_wasm.sh
@@ -6,12 +6,17 @@ latest_release=$(curl -s https://api.github.com/repos/streamdal/snitch-wasm/rele
 # Step 2: Extract the "browser_download_url" from the JSON response
 download_url=$(echo "$latest_release" | grep -o 'https://.*\.zip')
 
-# Step 3: Curl the download URL and save as release.zip
+# Step 3: Add debug info
+version=$(echo $download_url | cut -d / -f8)
+echo "WASM artifact version: ${version}" > src/version.txt
+echo "Last updated: $(date)" >> src/version.txt
+
+# Step 4: Curl the download URL and save as release.zip
 curl -L "$download_url" -o release.zip
 
-# Step 4: Unzip release.zip into the src/ directory
+# Step 5: Unzip release.zip into the src/ directory
 unzip -o release.zip -d src/
 
-# Clean up: Remove the downloaded zip file
+# Step 6: Clean up & info
 rm release.zip
-
+cat src/version.txt
