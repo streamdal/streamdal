16d5fce8b05109744ee8a3166dd15df966384a1b
Mark G <mark@gregan.org>
2023-09-13 16:22:20 -0400

    Update external_test.go

--- apis/grpcapi/external_test.go
+++ apis/grpcapi/external_test.go
@@ -823,6 +823,7 @@ func runServer() {
 		NotifyService:   d.NotifyService,
 		NATSBackend:     d.NATSBackend,
 		PubSubService:   d.PubSubService,
+		MetricsService:  d.MetricsService,
 	})
 
 	if err != nil {
