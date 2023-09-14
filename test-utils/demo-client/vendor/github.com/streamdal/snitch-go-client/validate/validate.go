868c084dd5367d878bc3b0e80431af2b9514af82
Daniel Selans <daniel.selans@gmail.com>
2023-09-14 14:05:00 -0700

    missed a vendor dir

---
+++ test-utils/demo-client/vendor/github.com/streamdal/snitch-go-client/validate/validate.go
@@ -0,0 +1,120 @@
+package validate
+
+import (
+	"github.com/pkg/errors"
+	"github.com/streamdal/snitch-protos/build/go/protos/shared"
+
+	"github.com/streamdal/snitch-protos/build/go/protos"
+)
+
+var (
+	ErrNilInput = errors.New("request cannot be nil")
+)
+
+func ErrEmptyField(field string) error {
+	return errors.Errorf("field '%s' cannot be empty", field)
+}
+
+func ErrNilField(field string) error {
+	return errors.Errorf("field '%s' cannot be nil", field)
+}
+
+func ErrUnsetEnum(field string) error {
+	return errors.Errorf("enum '%s' cannot be unset", field)
+}
+
+func Audience(aud *protos.Audience) error {
+	if aud == nil {
+		return ErrNilInput
+	}
+
+	if aud.ServiceName == "" {
+		return ErrEmptyField("service_name")
+	}
+
+	if aud.ComponentName == "" {
+		return ErrEmptyField("component_name")
+	}
+
+	if aud.OperationName == "" {
+		return ErrEmptyField("operation_name")
+	}
+
+	if aud.OperationType == protos.OperationType_OPERATION_TYPE_UNSET {
+		return ErrUnsetEnum("operation_type")
+	}
+
+	return nil
+}
+
+func KVInstruction(i *protos.KVInstruction) error {
+	if i == nil {
+		return errors.New("KVInstruction cannot be nil")
+	}
+
+	if i.Action == shared.KVAction_KV_ACTION_UNSET {
+		return errors.New("KVAction cannot be UNSET")
+	}
+
+	if i.Object == nil {
+		return errors.New("KVInstruction.Object cannot be nil")
+	}
+
+	return nil
+}
+
+func TailRequestStartCommand(cmd *protos.Command) error {
+	if cmd == nil {
+		return ErrNilInput
+	}
+
+	tail := cmd.GetTail()
+	if tail == nil {
+		return ErrNilField("tail")
+	}
+
+	req := tail.GetRequest()
+	if req == nil {
+		return ErrNilField("request")
+	}
+
+	if req.PipelineId == "" {
+		return ErrEmptyField("pipeline_id")
+	}
+
+	if err := Audience(cmd.Audience); err != nil {
+		return errors.Wrap(err, "invalid audience")
+	}
+
+	return nil
+}
+
+func TailRequestStopCommand(cmd *protos.Command) error {
+	if cmd == nil {
+		return ErrNilInput
+	}
+
+	tail := cmd.GetTail()
+	if tail == nil {
+		return ErrNilField("tail")
+	}
+
+	req := tail.GetRequest()
+	if req == nil {
+		return ErrNilField("request")
+	}
+
+	if req.Id == "" {
+		return ErrEmptyField("id")
+	}
+
+	return nil
+}
+
+func KVCommand(kv *protos.KVCommand) error {
+	if kv == nil {
+		return ErrNilInput
+	}
+
+	return nil
+}
