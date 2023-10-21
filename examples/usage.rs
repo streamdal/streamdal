34a0ed0e8a4c408bc1c9bc7fd8296b5036a61d16
Daniel Selans <daniel.selans@gmail.com>
2023-10-20 18:31:13 -0700

    renaming to include wasm prefix

--- examples/usage.rs
+++ examples/usage.rs
@@ -1,6 +1,6 @@
 use protos::sp_steps_detective::DetectiveType;
-use streamdal_detective::detective;
-use streamdal_detective::detective::Request;
+use streamdal_wasm_detective::detective;
+use streamdal_wasm_detective::detective::Request;
 
 fn main() {
     let det = detective::Detective::new();
