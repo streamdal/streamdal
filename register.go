afb718b07da7c2a10cee8db4df65e67f4d145d66
blinktag <blinktag@users.noreply.github.com>
2023-10-31 00:48:48 +0000

    Bumped version in register.go to v0.0.71

--- register.go
+++ register.go
@@ -25,7 +25,7 @@ func (s *Streamdal) genClientInfo() *protos.ClientInfo {
 	return &protos.ClientInfo{
 		ClientType:     protos.ClientType(s.config.ClientType),
 		LibraryName:    "go-sdk",
-		LibraryVersion: "v0.0.70",
+		LibraryVersion: "v0.0.71",
 		Language:       "go",
 		Arch:           runtime.GOARCH,
 		Os:             runtime.GOOS,
