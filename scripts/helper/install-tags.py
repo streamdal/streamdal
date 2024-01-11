8c5a333b86ef38fa38d2a2f3b2276d3920e7fbcd
Fritz Stauffacher <fritzstauff@gmail.com>
2024-01-11 11:36:50 -0500

    Updgrade Console 0.0.10 / Server 0.0.12 add ecs to helper script

--- scripts/helper/install-tags.py
+++ scripts/helper/install-tags.py
@@ -4,6 +4,8 @@ import re
 
 # File paths
 values_yaml = '../../docs/install/helm/streamdal-server/values.yaml'
+ecs_private_yaml = '../../docs/install/ecs/ecs-private.yml'
+ecs_public_yaml = '../../docs/install/ecs/ecs-public.yml'
 deployment_yaml = '../../docs/install/helm/streamdal-server/templates/streamdal-console-deployment.yaml'
 docker_compose_yaml = '../../docs/install/docker/docker-compose.yml'
 chart_yaml = '../../docs/install/helm/streamdal-server/Chart.yaml'
@@ -16,6 +18,23 @@ def get_latest_tag(repository):
     except subprocess.CalledProcessError as e:
         print(f"Error fetching latest tag for {repository}: {e}")
         return None
+
+def update_ecs_yml(file_path, server_tag, console_tag):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Patterns to match the full image line
+    server_image_pattern = r'Image: streamdal/server:[\w.-]+'
+    console_image_pattern = r'Image: streamdal/console:[\w.-]+'
+    print("Running update_ecs_yml")
+    # Replace the full image line
+    updated_content = re.sub(server_image_pattern, f'Image: streamdal/server:{server_tag}', content)
+    updated_content = re.sub(console_image_pattern, f'Image: streamdal/console:{console_tag}', updated_content)
+
+    with open(file_path, 'w') as file:
+        file.write(updated_content)
+
+
 
 def update_file_with_regex(file_path, pattern, new_tag, use_capture_groups=True):
     with open(file_path, 'r') as file:
@@ -57,6 +76,12 @@ if __name__ == "__main__":
         update_file_with_regex(deployment_yaml, console_image_pattern, f'{console_tag}')
         update_file_with_regex(docker_compose_yaml, r'image: streamdal/console:\S+', f'image: streamdal/console:{console_tag}', use_capture_groups=False)
         print(f"Updated console image tags in {deployment_yaml} and {docker_compose_yaml} to {console_tag}")
+
+    if server_tag and console_tag:
+        print("Updating ECS YAML files")
+        update_ecs_yml(ecs_private_yaml, server_tag, console_tag)
+        update_ecs_yml(ecs_public_yaml, server_tag, console_tag)
+        print(f"Updated server and console image tags in {ecs_private_yaml} and {ecs_public_yaml} to {server_tag} and {console_tag}")
 
     increment_chart_version(chart_yaml)
     print(f"Incremented Helm chart version in {chart_yaml}")
