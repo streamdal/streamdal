90163aa0fa236fb7064f461aff77c2a729ace901
Jacob Heric <jacob@jacobheric.com:q!>
2024-01-24 15:22:19 -0500

    wire up new step condition shape to the pipeline ui

---
+++ apps/console/components/form/radioGroup.tsx
@@ -0,0 +1,48 @@
+import { ErrorType, resolveValue } from "./validate.ts";
+import { useState } from "preact/hooks";
+
+export type RadioGroupProps = {
+  name: string;
+  data: any;
+  options: { [k: number]: string };
+  errors: ErrorType;
+  wrapperClass?: string;
+};
+
+export const RadioGroup = ({
+  name,
+  data,
+  options,
+  errors,
+  wrapperClass,
+}: RadioGroupProps) => {
+  const [selected, setSelected] = useState(resolveValue(data, name) || 0);
+  return (
+    <div class={`flex flex-col border rounded my-2 pt-2 px-2 pb-1 w-full`}>
+      <div
+        class={`flex flex-row justify-start items-center ${wrapperClass}`}
+      >
+        {Object.entries(options).map(([k, v]) => {
+          return (
+            <div class="flex flex-row items-center">
+              <input
+                type="radio"
+                name={name}
+                className={`w-4 h-4 rounded border mr-1 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2`}
+                value={k}
+                checked={selected == k}
+                onChange={() => setSelected(k)}
+              />
+              <label className="text-web font-medium text-[14px] mr-4">
+                {v}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+      <div className="text-[12px] mt-1 font-semibold text-streamdalRed">
+        {errors[name]}
+      </div>
+    </div>
+  );
+};
--- apps/console/components/pipeline/stepCondition.tsx
+++ apps/console/components/pipeline/stepCondition.tsx
@@ -1,37 +1,88 @@
 import { ErrorType } from "../form/validate.ts";
-import { CheckboxGroup } from "../form/checkboxGroup.tsx";
-import { PipelineStepCondition } from "streamdal-protos/protos/sp_pipeline.ts";
+import { AbortCondition } from "streamdal-protos/protos/sp_pipeline.ts";
+import { FormBoolean } from "../form/formBoolean.tsx";
+import { RadioGroup } from "../form/radioGroup.tsx";
+import { MetaData } from "./metadata.tsx";
+import IconInfoCircle from "tabler-icons/tsx/info-circle.tsx";
+import { Tooltip } from "../tooltip/tooltip.tsx";
+import { useEffect, useState } from "preact/hooks";
+import { initFlowbite } from "flowbite";
+import IconChevronUp from "tabler-icons/tsx/chevron-up.tsx";
+import IconChevronDown from "tabler-icons/tsx/chevron-down.tsx";
 
 export type StepConditionType = {
-  stepIndex: number;
+  name: string;
+  label: string;
+  toolTip: string;
   data: any;
-  setData: (data: any) => void;
   errors: ErrorType;
 };
 
-export const StepConditions = (
-  { stepIndex, data, setData, errors }: StepConditionType,
+export const StepCondition = (
+  { name, label, toolTip, data, errors }: StepConditionType,
 ) => {
+  const [expanded, setExpanded] = useState(false);
+
+  useEffect(() => {
+    initFlowbite();
+  }, []);
+
   return (
-    <div class="flex flex-row justify-between">
-      <CheckboxGroup
-        label="On Success"
-        name={`steps.${stepIndex}.onSuccess`}
-        data={data}
-        setData={setData}
-        errors={errors}
-        options={PipelineStepCondition}
-        wrapperClass="w-1/2 mr-1"
-      />
-      <CheckboxGroup
-        label="On Failure"
-        name={`steps.${stepIndex}.onFailure`}
-        data={data}
-        setData={setData}
-        errors={errors}
-        options={PipelineStepCondition}
-        wrapperClass="w-1/2 ml-1"
-      />
+    <div
+      class={`my-2 border rounded-sm ${
+        errors && Object.keys(errors).some((k) => k.includes(name)) &&
+        "border-streamdalRed"
+      }`}
+    >
+      <div
+        class="flex flex-row justify-between items-center p-2 cursor-pointer"
+        onClick={() => setExpanded(!expanded)}
+      >
+        <div class="flex flex-row justify-start items-center">
+          <label
+            className={`text-xs ${errors[name] && "text-streamdalRed"}`}
+          >
+            {label}
+          </label>
+          <IconInfoCircle
+            class="w-4 h-4 ml-1"
+            data-tooltip-target={`${name}-tooltip`}
+          />
+          <Tooltip
+            targetId={`${name}-tooltip`}
+            message={toolTip}
+          />
+        </div>
+        {expanded
+          ? <IconChevronUp class="w-6 h-6 text-twilight" />
+          : <IconChevronDown class="w-6 h-6 text-twilight" />}
+      </div>
+      <div
+        className={`flex flex-col p-2 w-full border-t md:${
+          expanded ? "visible" : "hidden"
+        }`}
+      >
+        <FormBoolean
+          name={`${name}.notify`}
+          data={data}
+          display="Notify"
+        />
+        <RadioGroup
+          name={`${name}.abort`}
+          data={data}
+          errors={errors}
+          options={{
+            [AbortCondition.UNSET]: "Don't Abort",
+            [AbortCondition.ABORT_CURRENT]: "Abort Current Pipeline",
+            [AbortCondition.ABORT_ALL]: "Abort All Pipelines",
+          }}
+        />
+        <MetaData
+          name={`${name}.metadata`}
+          data={data}
+          errors={errors}
+        />
+      </div>
     </div>
   );
 };
