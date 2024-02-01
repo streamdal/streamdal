a5a8e71309a315d0a9e9047ca5a97ba1726f00e8
blinktag <blinktag@users.noreply.github.com>
2024-02-01 20:16:32 +0000

    Bumped version in setup.py to 0.0.45

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(
     name="streamdal",
-    version="0.0.44",
+    version='0.0.45',
     description="Python client SDK for Streamdal's open source observability server",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- streamdal/__init__.py
+++ streamdal/__init__.py
@@ -616,7 +616,7 @@ class StreamdalClient:
         return protos.ClientInfo(
             client_type=protos.ClientType(self.cfg.client_type),
             library_name="python-sdk",
-            library_version="0.0.44",
+            library_version="0.0.45",
             language="python",
             arch=platform.processor(),
             os=platform.system(),
