8e62aeeb7882a87c5443f86a7ce10c4eccd362db
dselans <dselans@users.noreply.github.com>
2024-02-09 07:08:21 +0000

    Bumped version in setup.py to 0.1.4

--- sdks/python/streamdal/__init__.py
+++ sdks/python/streamdal/__init__.py
@@ -588,7 +588,7 @@ class StreamdalClient:
         return protos.ClientInfo(
             client_type=protos.ClientType(self.cfg.client_type),
             library_name="python-sdk",
-            library_version="0.1.3",
+            library_version="0.1.4",
             language="python",
             arch=platform.processor(),
             os=platform.system(),
