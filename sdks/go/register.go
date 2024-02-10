139a1fa9d6bddcc03471ad34fd438d4190e3024c
dselans <dselans@users.noreply.github.com>
2024-02-10 01:32:26 +0000

    Bumped version in register.go to 0.1.4

--- sdks/go/register.go
+++ sdks/go/register.go
@@ -19,7 +19,7 @@ func (s *Streamdal) genClientInfo() *protos.ClientInfo {
 	return &protos.ClientInfo{
 		ClientType:     protos.ClientType(s.config.ClientType),
 		LibraryName:    "go-sdk",
-		LibraryVersion: "0.1.3",
+		LibraryVersion: "0.1.4",
 		Language:       "go",
 		Arch:           runtime.GOARCH,
 		Os:             runtime.GOOS,
