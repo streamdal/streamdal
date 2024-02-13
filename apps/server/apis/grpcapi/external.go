8d72e8bd85544b27f6bde9db73ccc4d30932c9f6
Daniel Selans <daniel.selans@gmail.com>
2024-02-13 10:25:19 -0800

    skeleton for deletepipelineconfig

--- apps/server/apis/grpcapi/external.go
+++ apps/server/apis/grpcapi/external.go
@@ -475,12 +475,21 @@ func (s *ExternalServer) DeletePipeline(ctx context.Context, req *protos.DeleteP
 			return util.StandardResponse(ctx, protos.ResponseCode_RESPONSE_CODE_NOT_FOUND, err.Error()), nil
 		}
 
-		return util.StandardResponse(ctx, protos.ResponseCode_RESPONSE_CODE_INTERNAL_SERVER_ERROR, err.Error()), nil
+		return util.StandardResponse(ctx, protos.ResponseCode_RESPONSE_CODE_INTERNAL_SERVER_ERROR,
+			"unable to fetch existing pipeline: "+err.Error()), nil
 	}
 
 	// Pipeline exists, delete it
 	if err := s.Options.StoreService.DeletePipeline(ctx, req.PipelineId); err != nil {
-		return util.StandardResponse(ctx, protos.ResponseCode_RESPONSE_CODE_INTERNAL_SERVER_ERROR, err.Error()), nil
+		return util.StandardResponse(ctx, protos.ResponseCode_RESPONSE_CODE_INTERNAL_SERVER_ERROR,
+			"unable to delete pipeline: "+err.Error()), nil
+	}
+
+	// Delete pipeline config (we can safely delete because bus handler will just
+	// emit a SetPipelines cmd without the pipeline config)
+	if _, err := s.Options.StoreService.DeletePipelineConfig(ctx, req.PipelineId); err != nil {
+		return util.StandardResponse(ctx, protos.ResponseCode_RESPONSE_CODE_INTERNAL_SERVER_ERROR,
+			"unable to delete pipeline config(s): "+err.Error()), nil
 	}
 
 	// Send telemetry
