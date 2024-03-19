f2af341e38cc8d0cd38211e16fd831096a26f2a1
blinktag <blinktag@users.noreply.github.com>
2024-03-19 15:05:07 +0000

    Bumped version in setup.py to 0.1.11

--- sdks/python/streamdal/__init__.py
+++ sdks/python/streamdal/__init__.py
@@ -594,7 +594,7 @@ class StreamdalClient:
         return protos.ClientInfo(
             client_type=protos.ClientType(self.cfg.client_type),
             library_name="python-sdk",
-            library_version="0.1.10",
+            library_version="0.1.11",
             language="python",
             arch=platform.processor(),
             os=platform.system(),
