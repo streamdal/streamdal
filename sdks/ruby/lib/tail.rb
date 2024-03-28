e5d4131797ff8613d7c435ce3847c5384eae0bef
Mark G <mark@gregan.org>
2024-03-28 15:24:04 -0400

    initial work on tail modules

---
+++ sdks/ruby/lib/tail.rb
@@ -0,0 +1,69 @@
+# TODO: implement token bucket limiter
+require "bozos_buckets"
+
+NUM_TAIL_WORKERS = 2
+MIN_TAIL_RESPONSE_INTERVAL_MS = 100
+
+module Streamdal
+  class Tail
+    attr_accessor :queue, :active
+
+    def initialize(request, streamdal_url, auth_token, log, metrics, active = false)
+      @request = request
+      @streamdal_url = streamdal_url
+      @auth_token = auth_token
+      @log = log
+      @metrics = metrics
+      @active = active
+      @last_msg = Time::at(0)
+      @queue = Queue.new
+
+      # Only use rate limiting if sample_options is set
+      unless request.sample_options.nil?
+        @limiter = BozosBuckets::Bucket(
+          initial_token_count: request.sample_options.sample_rate,
+          refill_rate: request.sample_options.sample_interval_seconds,
+          max_token_count: request.sample_options.sample_rate
+        )
+      end
+    end
+
+    def start_tail_workers
+      NUM_TAIL_WORKERS.times do |worker_id|
+        Thread.new do
+          start_tail_worker(worker_id + 1)
+        end
+      end
+    end
+
+    def start_tail_worker(worker_id)
+      @logger.debug("Starting tail worker #{worker_id}")
+
+      # Create gRPC connection
+      @stub = Streamdal::Protos::Internal::Stub.new(@streamdal_url, :this_channel_is_insecure)
+
+      while @active && !@queue.empty?
+        if Time::now - @last_msg < MIN_TAIL_RESPONSE_INTERVAL_MS
+          sleep(MIN_TAIL_RESPONSE_INTERVAL_MS)
+
+          # TODO: metric for COUNTER_DROPPED_TAIL_MESSAGES
+          @logger.debug("Dropped tail message for '#{@request.id}' due to rate limiting")
+          next
+        end
+
+        @logger.debug("Sending tail request for '#{tail_response.tail_request_id}'")
+        tail_response = @queue.pop(non_block = false)
+        @stub.tail(tail_response, metadata: { "auth-token" => @auth_token })
+
+      end
+
+      def should_send
+        if @limiter.nil?
+          nil
+        end
+
+        @limiter.use_tokens(1)
+      end
+    end
+  end
+end
