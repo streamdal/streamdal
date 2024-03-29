882232002184a11069d7f87e14be962a2a3ed25c
Mark G <mark@gregan.org>
2024-03-29 09:22:18 -0400

    Update demo.rb

--- sdks/ruby/demo.rb
+++ sdks/ruby/demo.rb
@@ -1,7 +1,7 @@
 require "./lib/streamdal"
 
-a = Streamdal::Client.new({ service_name: "demo-ruby", streamdal_token: "1234" })
-a.tmp
+a = Streamdal::Client.new({ service_name: "demo-ruby", streamdal_token: "1234", streamdal_url: "localhost:8082" })
+# a.tmp
 
 puts "done"
 
