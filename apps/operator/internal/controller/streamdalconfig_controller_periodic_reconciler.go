e753aa4cc278b94cf9cb052d6f2aa83d3116e6cf
Daniel Selans <daniel.selans@gmail.com>
2024-04-21 19:53:10 -0700

    forgot the actual periodic reconciler file

---
+++ apps/operator/internal/controller/streamdalconfig_controller_periodic_reconciler.go
@@ -0,0 +1,87 @@
+package controller
+
+import (
+	"fmt"
+	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	crdv1 "github.com/streamdal/streamdal/apps/operator/api/v1"
+)
+
+// runPeriodicReconciler is intended to be ran as a goroutine, started at operator
+// startup. It will run every DefaultReconcileInterval or CR.periodicReconcilerInterval
+// (if set).
+//
+// The periodic reconciler is designed to periodically check and ensure that
+// the state of a system aligns with its desired state. It automatically
+// corrects discrepancies by triggering necessary updates or fixes. This
+// mechanism is essential for maintaining system integrity and reliability over
+// time.
+//
+// A periodic reconciler is needed in addition to the regular reconcile loop to
+// handle cases where external changes or system errors might not trigger events
+// detected by the regular loop. For example: someone manually removed a config
+// entry in the streamdal server via the UI - the periodic reconciler would detect
+// the discrepancy and correct it by re-adding the config entry.
+func (r *StreamdalConfigReconciler) runPeriodicReconciler() {
+	llog := log.Log.WithValues("method", "runPeriodicReconciler")
+	llog.Info("Starting")
+
+	ticker := time.NewTicker(r.periodicReconcilerInterval)
+
+MAIN:
+
+	for {
+		select {
+		case <-r.ShutdownCtx.Done():
+			llog.Info("Caught shutdown signal - stopping")
+			break MAIN
+		case <-ticker.C:
+			if err := r.runPeriodicReconcilerIteration(); err != nil {
+				llog.Error(err, "Failed to run periodic reconciler iteration")
+			}
+		}
+	}
+
+	llog.Info("Stopped")
+}
+
+func (r *StreamdalConfigReconciler) runPeriodicReconcilerIteration() error {
+	llog := log.Log.WithValues("method", "runPeriodicReconcilerIteration")
+
+	llog.Info("Fetching list of CRs")
+
+	// Get a list of CR's
+	crs := &crdv1.StreamdalConfigList{}
+
+	if err := r.List(r.ShutdownCtx, crs); err != nil {
+		return fmt.Errorf("failed to list StreamdalConfig objects: %s", err)
+	}
+
+	if len(crs.Items) == 0 {
+		llog.Info("No CRs to reconcile")
+		return nil
+	}
+
+	llog.Info("Going to reconcile CRs", "numCRs", len(crs.Items))
+
+	var numReconciled int
+
+	// For each CR, verify the config is correct
+	for _, cr := range crs.Items {
+		rr, err := r.setupReconcileAction(r.ShutdownCtx, nil, &cr)
+		if err != nil {
+			return fmt.Errorf("failed to setup reconcile action for CR '%s': %s", cr.Name, err)
+		}
+
+		if _, err := r.handleResources(r.ShutdownCtx, rr); err != nil {
+			return fmt.Errorf("failed to handle resources for CR '%s': %s", cr.Name, err)
+		}
+
+		llog.Info("CR reconciled", "crName", cr.Name)
+		numReconciled += 1
+	}
+
+	return nil
+}
