622a81b432fcfaeac79af79d8fed4093e04f0bfd
Mark G. <mark@gregan.org>
2024-05-03 17:13:22 -0500

    Add body_mode

--- libs/wasm/httprequest/src/lib.rs
+++ libs/wasm/httprequest/src/lib.rs
@@ -47,6 +47,7 @@ pub extern "C" fn f(ptr: *mut u8, length: usize) -> u64 {
                             method: wasm_request.step.http_request().request.method,
                             headers: wasm_request.step.http_request().request.headers.clone(),
                             body: wasm_request.input_payload.clone(),
+                            body_mode: wasm_request.step.http_request().request.body_mode,
                             ..Default::default()
                         }
                     )
