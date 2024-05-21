d028a19ee75121c9d6366f6a3268b4d0fe230ad2
uzarubin <daedaux@gmail.com>
2024-05-21 13:32:24 -0400

    reducing config download size by removing empty values

--- apps/console/routes/configs.tsx
+++ apps/console/routes/configs.tsx
@@ -1,11 +1,28 @@
 import { Handlers } from "$fresh/server.ts";
 import { getConfigs } from "root/lib/fetch.ts";
+import "lodash";
+
+const cleanObject = (obj: any): any => {
+  const keysToRemove = ["WasmId", "WasmFunction"];
+
+  if (_.isArray(obj)) {
+    return _.compact(obj.map(cleanObject)).filter((o) => !_.isEmpty(o));
+  } else if (_.isObject(obj)) {
+    return _.omitBy(
+      _.mapValues(_.omit(obj, keysToRemove), cleanObject),
+      (value) => _.isEmpty(value) && !_.isNumber(value),
+    );
+  }
+  return obj;
+};
 
 export const handler: Handlers = {
   async GET() {
     const { config = {} } = await getConfigs();
+
+    const cleanedConfig = cleanObject(config);
     return new Response(
-      JSON.stringify(config, undefined, 2),
+      JSON.stringify(cleanedConfig, undefined, 2),
       {
         headers: {
           "Content-Type": "application/json",
