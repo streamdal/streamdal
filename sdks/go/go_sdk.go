c05fb0ad58c4e6fd06e044660fa3fc8b1bcbc109
Mark G. <mark@gregan.org>
2024-05-23 11:36:34 -0500

    print cachedir

--- sdks/go/go_sdk.go
+++ sdks/go/go_sdk.go
@@ -419,6 +419,8 @@ func New(cfg *Config) (*Streamdal, error) {
 		s.config.Logger.Error(err)
 	}
 
+	println("CACHE DIR: ", cacheDir)
+
 	// Initializes the new compilation cache with the cache directory.
 	// This allows the compilation caches to be shared even across multiple OS processes.
 	cache, err := wazero.NewCompilationCacheWithDir(cacheDir)
