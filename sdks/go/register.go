8b7da1989115e04863bf2cb3199068f9c758c57f
Mark G. <mark@gregan.org>
2024-05-30 10:19:42 -0500

    Update register.go

--- sdks/go/register.go
+++ sdks/go/register.go
@@ -20,7 +20,7 @@ func (s *Streamdal) genClientInfo() *protos.ClientInfo {
 	return &protos.ClientInfo{
 		ClientType:     protos.ClientType(s.config.ClientType),
 		LibraryName:    "go-sdk",
-		LibraryVersion: "0.1.32-wasmtime",
+		LibraryVersion: "0.1.31-wasmtime",
 		Language:       "go",
 		Arch:           runtime.GOARCH,
 		Os:             runtime.GOOS,
